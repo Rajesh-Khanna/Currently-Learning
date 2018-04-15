daedd263b8a157a00026026b3fab2aefae7222f3
Rajesh-Khanna <chrajeshkhanna82798@gmail.com>
2018-04-15 15:20:57 +0530

    VISCOM complete

--- Web Development/VISCOM/sketch.js
+++ Web Development/VISCOM/sketch.js
@@ -3,8 +3,9 @@ var sw = 800,sh = 500;
 function setup() {
 //	cnv = createCanvas(sw,sh);
 //	cnv.parent('drawboard');
+//  cnv.style('pointer-events','none');
 }
 
 function draw() {
-//	cnv.background('#000000');
+//	cnv.background('#ffffff');
 }
